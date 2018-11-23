6f8cddd411cad9235a1b5f64bac1371fb0e92bbc
Myhailo Deba <darklord793@gmail.com>
2018-11-23 13:38:07 +0000

    initial

---
+++ js/index.js
@@ -0,0 +1,50 @@
+const a = prompt('Enter position number in the Fibonacci line. To calculate in Сycle');
+let resultСycle = fibonacciСycle(a);
+alert(resultСycle);
+
+const b = prompt('Enter position number in the Fibonacci line. To calculate in Massif');
+let resultMassif = fibonacciMassif(b);
+alert(resultMassif);
+
+const c = prompt('Enter position number in the Fibonacci line. To calculate in Recursion');
+let resultRecursion = fibonacciRecursion(с);
+alert(resultRecursion);
+
+function fibonacciСycle(a) {
+    let first = 1;
+    let second = 1;
+    let next = 0;
+    if (a === 1) {
+        return "Under the number n :" + a + ", in the Fibonacci line is the number :" + first;
+    } else if (a === 2) { 
+        return "Under the number n :" + a + ", in the Fibonacci line is the number :" + second;
+    } else {
+    for (let i = 3; i <=a; i++) {
+    let next = first + second;
+    first = second;
+    second = next;
+    } 
+    return "Under the number n :" + a + ", in the Fibonacci line is the number :" + next;
+}
+}
+
+    let fibonacciMassif= [0, 1, 1,];
+    if (b === 1) {
+    alert( "Under the number n :" + b + ", in the Fibonacci line is the number :" + fibonacciMassif[1]);
+    } else if (b === 2) { 
+    alert( "Under the number n :" + b + ", in the Fibonacci line is the number :" + fibonacciMassif[2]);
+    } else {
+    for (let i = 3; i <= b; i++) {
+    fibonacciMassif[i].push(fibonacciMassif[i - 1] + fibonacciMassif[i - 2]);
+    alert( "Under the number n :" + b + ", in the Fibonacci line is the number :" + fibonacciMassif[i]);
+    }
+
+    function fibonacciRecursion(с) {
+    if (c === 1) {
+        return 1;
+    } else if (c === 2) { 
+        return 1;
+    } else {
+    return fibonacciRecursion(c - 1) + fibonacciRecursion(c - 2);
+    }
+    }
